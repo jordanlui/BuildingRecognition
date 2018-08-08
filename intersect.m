4c8de9ab57dd22e502c3da5ef2473352ef264101
jordan.lui@gmail.com <jordan.lui@gmail.com>
2018-08-07 18:35:18 -0700

    Removing last of old functions for cleanliness

--- intersect.m
+++
@@ -1,6 +0,0 @@
-function [x,y] = intersect(A)
-% Finds intersection between two sets of lines. Inputs are the slope and
-% y-intercept data of the line, as well as the upper and lower x bounds
-% which signify the domain on which the line exists
-
-end
